d216b76554793bab4a2c781f17335e9728bbea7d
David Mai <40285081+dmai261@users.noreply.github.com>
2019-04-21 03:12:54 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3004;
 
 app.use('/:id', express.static('public'));
-console.log('lol')
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
